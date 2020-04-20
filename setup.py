0a83de058d85a180caca1def8694317ef04ba55a
menziess <stefan_schenk@hotmail.com>
2020-04-20 23:01:25 +0200

    Removed useless variable package name.

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 """Setuptools script."""
 
-import os
-
 from setuptools import find_packages, setup
 
 if __name__ == '__main__':
-    package_name = os.getenv('PACKAGE_NAME')
     setup(
-        name=package_name,
+        name='devops-exporter',
         version='0.0.dev0',
         package_dir={'': 'src'},
         packages=find_packages('src', include=[
-            f'{package_name}*'
+            'devops_exporter*'
         ]),
         install_requires=[]
     )
