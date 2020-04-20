fa1891904cfd169423682dc1688f32cd5fcc4573
menziess <stefan_schenk@hotmail.com>
2020-04-20 23:11:44 +0200

    Added install requires.

--- setup.py
+++ setup.py
@@ -10,5 +10,18 @@ if __name__ == '__main__':
         packages=find_packages('src', include=[
             'devops_exporter*'
         ]),
-        install_requires=[]
+        install_requires=[
+            'azure-devops==6.0.0b2',
+            'certifi==2020.4.5.1',
+            'chardet==3.0.4',
+            'idna==2.9',
+            'isodate==0.6.0',
+            'msrest==0.6.13',
+            'oauthlib==3.1.0',
+            'python-dotenv==0.13.0',
+            'requests-oauthlib==1.3.0',
+            'requests==2.23.0',
+            'six==1.14.0',
+            'urllib3==1.25.9'
+        ]
     )
